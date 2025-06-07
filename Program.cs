653e91ef4159e890a78be1a00d3ab96a09ce8333
duridev <duridev@outlook.com>
2025-06-07 01:18:36 -0400

    desarrollo de ejercicio

--- Program.cs
+++ Program.cs
@@ -6,7 +6,34 @@ namespace mod3_ex4
     {
         static void Main(string[] args)
         {
+            int n1, n2, n3, n4;
 
+            Console.WriteLine("Escribe un número:");
+            n1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Escribe un número:");
+            n2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Escribe un número:");
+            n3 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Escribe un número:");
+            n4 = int.Parse(Console.ReadLine());
+
+            if (n1 < n2 && n1 < n3 && n1 < n4)
+            {
+                Console.WriteLine($"{n1} es el menor de los cuatro números");
+            }
+            else if (n2 < n3 && n2 < n4)
+            {
+                Console.WriteLine($"{n2} es el menor de los cuatro números");
+            }
+            else if (n3 < n4)
+            {
+                Console.WriteLine($"{n3} es el menor de los cuatro números");
+            }
+            else
+            {
+                Console.WriteLine($"{n4} es el menor de los cuatro números");
+            }
         }
     }
 }
